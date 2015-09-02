3b9b81213560e1f54f67eded5e64d5d8b4ebf6c0
shivloka <shiv.loka@appdynamics.com>
2015-09-02 11:15:57 -0700

    rectified query syntax error

--- src/main/java/com/dbwrapper/StatementExecutor.java
+++ src/main/java/com/dbwrapper/StatementExecutor.java
@@ -38,7 +38,7 @@ public class StatementExecutor {
 		logger.info("executing join query" + " " + flag + " " + dbName);
 		queryString = "select c.name, cl.language from world.country c, world.countrylanguage cl "
 				+ "where c.code = cl.countrycode and c.name = 'United States'";
-		slowQueryString = "select c.name, c.capital cl.language, cl.percentage, cu.customerid, "
+		slowQueryString = "select c.name, c.capital, cl.language, cl.percentage, cu.customerid, "
 				+ "cu.address from world.country c, world.countrylanguage cl, world.customers cu where c.name = 'United States'";
 
 		// checks if the slow query flag is set and
